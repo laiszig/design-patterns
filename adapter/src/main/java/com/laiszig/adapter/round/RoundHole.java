b5c34bfc67babee67dfaae6207fcfbcb6157d5b4
laiszig <laisziegler@gmail.com>
2025-01-31 17:57:38 -0300

    adapter pattern example

---
+++ adapter/src/main/java/com/laiszig/adapter/round/RoundHole.java
@@ -0,0 +1,22 @@
+package com.laiszig.adapter.round;
+
+/**
+ * Round holes are compatible with round pegs
+ */
+public class RoundHole {
+    private double radius;
+
+    public RoundHole(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public boolean fits(RoundPeg peg) {
+        boolean result;
+        result = (this.getRadius() >= peg.getRadius());
+        return result;
+    }
+}
---
+++ adapter/src/main/java/com/laiszig/adapter/round/RoundPeg.java
@@ -0,0 +1,20 @@
+package com.laiszig.adapter.round;
+
+/**
+ * Round pegs are compatible with round holes
+ */
+public class RoundPeg {
+
+    private double radius;
+
+    public RoundPeg() {
+    }
+
+    public RoundPeg(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
