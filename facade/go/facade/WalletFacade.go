373f84da52881d3de90b97d9a938aff9cf91cc77
laiszig <laisziegler@gmail.com>
2025-03-16 14:43:39 -0300

    facade go project

---
+++ facade/go/facade/WalletFacade.go
@@ -0,0 +1,65 @@
+package facade
+
+import "fmt"
+
+/*
+*
+Facade pattern: a thing that lets the client work with dozens of components using a simple interface
+*/
+type WalletFacade struct {
+	account      *Account
+	wallet       *Wallet
+	securityCode *SecurityCode
+	notification *Notification
+	ledger       *Ledger
+}
+
+func newWalletFacade(accountID string, code int) *WalletFacade {
+	fmt.Println("Starting account creation")
+	walletFacade := &WalletFacade{
+		account:      newAccount(accountID),
+		securityCode: newSecurityCode(code),
+		wallet:       newWallet(),
+		notification: &Notification{},
+		ledger:       &Ledger{},
+	}
+	fmt.Println("Account created")
+	return walletFacade
+}
+
+func (w *WalletFacade) addMoneyToWallet(accountID string, securityCode int, amount int) error {
+	fmt.Println("Starting Deposit")
+	err := w.account.checkAccount(accountID)
+	if err != nil {
+		return err
+	}
+	err = w.securityCode.checkCode(securityCode)
+	if err != nil {
+		return err
+	}
+
+	w.wallet.creditBalance(amount)
+	w.notification.sendWalletCreditNotification()
+	w.ledger.makeEntry(accountID, "credit", amount)
+	return nil
+}
+
+func (w *WalletFacade) deductMoneyFromWallet(accountID string, securityCode int, amount int) error {
+	fmt.Println("Starting debit money from wallet")
+	err := w.account.checkAccount(accountID)
+	if err != nil {
+		return err
+	}
+
+	err = w.securityCode.checkCode(securityCode)
+	if err != nil {
+		return err
+	}
+	err = w.wallet.debitBalance(amount)
+	if err != nil {
+		return err
+	}
+	w.notification.sendWalletDebitNotification()
+	w.ledger.makeEntry(accountID, "debit", amount)
+	return nil
+}
---
+++ facade/go/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"facade" // Import the package correctly
+)
+
+func main() {
+	fmt.Println()
+	walletFacade := newWalletFacade("abc", 1234)
+	fmt.Println()
+
+	err := walletFacade.addMoneyToWallet("abc", 1234, 10)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
+
+	fmt.Println()
+	err = walletFacade.deductMoneyFromWallet("abc", 1234, 5)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
+}
